5f618a351ff158cb7af103c51cb37b32e928bf0d
Aurélien Perrier <a.perrier89@gmail.com>
2017-11-26 11:56:50 +0100

    Changing output logs httpd

--- docker-images/httpd/scripts/start.sh
+++ docker-images/httpd/scripts/start.sh
@@ -21,5 +21,8 @@ echo "Clearing any old processes..."
 rm -f /run/apache2/apache2.pid
 rm -f /run/apache2/httpd.pid
 
+echo "Updating HTTPD config"
+sed -i "s/ErrorLog logs\/error.log/Errorlog \/dev\/stderr\nTransferlog \/dev\/stdout/" /etc/apache2/httpd.conf
+
 echo "Starting all process ..."
 exec httpd -DFOREGROUND
