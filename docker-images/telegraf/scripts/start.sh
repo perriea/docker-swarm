254ed510b24c714ac32725e1c03b4c9ce5e6d5dd
Aurélien Perrier <a.perrier89@gmail.com>
2017-12-01 11:10:19 +0100

    Fixing config plugin MySQL Telegraf

--- docker-images/telegraf/scripts/start.sh
+++ docker-images/telegraf/scripts/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo "Configuration Telegraf MySQL ..."
-sed -i "s/user:passwd/$MYSQL_USER_TELEGRAF:$MYSQL_USER_TELEGRAF_PWD/" /root/telegraf.conf
-
 echo "Starting Telegraf ..."
 ./telegraf/telegraf --config telegraf.conf
